31dfe1734ed24a6f6b01d93f3b5cff31570376b5
George Raptis <georapbox@gmail.com>
2024-11-06 12:56:49 +0200

    Add Jotai and related dependencies; configure Next.js for Jotai DevTools

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Learn more here - https://nextjs.org/docs/advanced-features/compiler#module-transpilation
+  // Required for UI css to be transpiled correctly
+  transpilePackages: ['jotai-devtools']
+};
 
 export default nextConfig;
---
+++ src/lib/ui-components/JotaiDevTools.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { DevTools } from 'jotai-devtools';
+import 'jotai-devtools/styles.css';
+
+export const JotaiDevTools = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    setTheme(mediaQuery.matches ? 'dark' : 'light');
+
+    const handleChange = () => {
+      setTheme(mediaQuery.matches ? 'dark' : 'light');
+    };
+
+    mediaQuery.addEventListener('change', handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleChange);
+    };
+  }, []);
+
+  return <DevTools theme={theme} isInitialOpen />;
+};
